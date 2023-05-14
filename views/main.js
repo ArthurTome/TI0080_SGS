d52aa406e20f9e4413b3ea92ed81df2bd4119275
NeanderDanubio26827 <77577250+NeanderDanubio26827@users.noreply.github.com>
2023-05-14 14:22:04 -0300

    criação de arquivos html

--- views/main.js
+++ views/main.js
@@ -18,31 +18,34 @@ function NigthMode(event) {
     var color = "rgb(" + 255 + ", " + 255 + ", " + 255 + ")";
     document.body.style.backgroundColor = color;
   }
-}
+};
 
 function aumentarTamanho() {
   var objeto = document.getElementById("header_index");
   var width = objeto.offsetWidth;
   var height = objeto.offsetHeight;
-  //objeto.style.width = width + 10 + "px";
-  objeto.style.height = height + 10 + "px";
-  var texto = document.getElementById("header_text");
-  var fontSize = parseInt(window.getComputedStyle(texto).fontSize);
+  var tex1 = document.getElementById("header_text", "footer");
+  //var tex2 = document.getElementsByClassName("social_icons");
+  var fontSize = parseInt(window.getComputedStyle(tex1).fontSize);
+  //var fontSize2 = parseInt(window.getComputedStyle(tex2).fontSize);
 
   //objeto.style.width = (width + 10) + "px";
   objeto.style.height = height + 10 + "px";
-  texto.style.fontSize = fontSize + 2 + "px";
-}
+  tex1.style.fontSize = fontSize + 2 + "px";
+
+
+};
+
 
 function diminuirTamanho() {
   var objeto = document.getElementById("header_index");
   var width = objeto.offsetWidth;
   var height = objeto.offsetHeight;
-  //objeto.style.width = width - 10 + "px";
+  var tex1 = document.getElementById("header_text", "footer");
+  //var tex2 = document.getElementsByClassName("social_icons");
+  var fontSize = parseInt(window.getComputedStyle(tex1).fontSize);
+  
   objeto.style.height = height - 10 + "px";
-  var texto = document.getElementById("header_text");
-  var fontSize = parseInt(window.getComputedStyle(texto).fontSize);
-  //objeto.style.width = (width - 10) + "px";
-  objeto.style.height = height - 10 + "px";
-  texto.style.fontSize = fontSize - 2 + "px";
-}
+  tex1.style.fontSize = fontSize - 2 + "px";
+  
+};
