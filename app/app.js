73e10cad41c24b1f786d31da5e400ea114ecb77b
Arthur Tome <arthurtomelopes@gmail.com>
2023-06-19 18:49:55 -0300

    login structure create

--- app/app.js
+++ app/app.js
@@ -12,7 +12,7 @@ const crypto = require('crypto')
 const jwt = require('jsonwebtoken')
 
 require('dotenv').config({path: __dirname + '/.env'});
-//const uuid = require(uuid).v4                 // GERA CARACTERES ALEATORIOS
+const uuid = require('uuid')                 // GERA CARACTERES ALEATORIOS
 
 //const { randomInt } = require('crypto');
 //var logger = require('morgan');
@@ -43,28 +43,88 @@ var sha512 = (pwd, salt) => {
     return hash.digest('hex') 
 }
 
-// PUXA O TOKEN DO HEADER (TEM QUE CONFIGURAR NO JWT)
-// AINDA ESTUDANDO COMO FUNCIONA
-function verifyJWT(req, res, next) {
-    const token = req.headers['x-access-token']
+// FUNÇÃO DO LADO DO BANCO DE DADOS
+function login(username, password) {
+    var users = JSON.parse(fs.readFileSync(__dirname + '/db/cli.json'))
+    var userloc = users.find((item) => {            // BUSCA NO BANCO DE DADOS
+        return ((item.user == username)&&(item.password == password))
+    })
 
-    // verifica se o token foi incluindo na blacklist devido a logout
-    const index = blacklist.findIndex(item => item === token)
- 
-    if (index !== -1) {// está na blacklist!
-        console.log('### 1 - está na blacklist')
-        res.status(401).end('está na blacklist - fazer login novamente!') 
-    } else {
-        jwt.verify(token, process.env.SECRET, (err, decoded) => {
-            if (err) {
-                console.log('### 2 - erro na verificação')
-                res.status(403).end('token inválido - fazer login novamente');
-            } else {
-                req.userid = decoded.userid;
-                next();
-            }
-        })
+    if (userloc){
+        return true;
     }
+    return false;
+}
+
+// VERIFICA TOKEN NO BANCO DE DADOS
+function token_verify(req, res, next) {
+    var session = JSON.parse(fs.readFileSync(__dirname + '/db/session.json'))  // db de token
+    var user_check = session.findIndex((item) => {
+        return (item.sesion_token == req.cookies.user_token)
+    })
+    if (user_check == -1) {                                 // SEM TOKEN ASSOCIADO - > NOVO LOGIN
+        return res.status(401).redirect('login')
+    }
+    next();
+}
+
+function token_consut(token) {
+    var session = JSON.parse(fs.readFileSync(__dirname + '/db/session.json'))  // db de token
+    var user = session.find((item) => {
+        return (item.sesion_token == token)
+    })
+
+    return user.user
+}
+
+// CRIA OU MODIFICA TOKEN NO BANCO DE DADOS
+function token_modify(user) {
+    var sesion_token;
+    var session = JSON.parse(fs.readFileSync(__dirname + '/db/session.json'));
+    var user_check = session.findIndex((item) => {
+        return (item.user == user)
+    })
+
+    console.log(user_check)
+    if (user_check == -1){                               // USUARIO NÃO EXISTE
+        do{
+            sesion_token = uuid.v4();
+            var session_check = session.findIndex((item) => {
+                return (item.sesion_token == sesion_token)
+            })
+        }while(session_check != -1);
+        
+        session.push({
+            "user": user,
+            "sesion_token": sesion_token
+        }) 
+        fs.writeFileSync(__dirname + '/db/session.json',
+            JSON.stringify(session, null, 2)
+        )
+    } else {                                            // USUARIO JA EXISTE
+        do{
+            sesion_token = uuid.v4();
+            var session_check = session.findIndex((item) => {
+                return (item.sesion_token == sesion_token)
+            })
+        }while(session_check != -1);
+
+        session[user_check].sesion_token = sesion_token;
+
+        fs.writeFileSync(__dirname + '/db/session.json',
+        JSON.stringify(session, null, 2)
+        )
+    }
+
+    return sesion_token;
+}
+
+function userdata(user){
+    var users_data = JSON.parse(fs.readFileSync(__dirname + '/db/cli_data.json'))
+    var data = users_data.find((item) => {            // BUSCA NO BANCO DE DADOS
+        return (item.user == user)
+    })
+    return data;
 }
 
 // CRIA USUARIO COM SENHA CONDIFICADA E INSERE NA BASE DE DADOS
@@ -98,6 +158,7 @@ function create_user(userid, user, password) {
 
 // --------------------| CRIA ROTAS |--------------------
 app.get('/', (req, res) => {
+    // 1. BUSCA POR INFORMACOES PARA EXIBIR EM (NOTICES)
     res.status(200).render('home');             // PROCURA ARQUIVO home.hbs
 })
 
@@ -109,18 +170,41 @@ app.get('/create', (req, res) => {
     res.status(200).render('create');           // PROCURA ARQUIVO create.hbs
 })
 
-app.get('/users', (req, res) => {
-    console.log(req.cookies.user_token);
+app.get('/users', token_verify, (req, res) => {
+    // 1. VERIFICA TOKEN DE LOGIN EM (USER_SESSIONS)
+    //  1.1 ERRO REDIRECIONA 'LOGIN'
+    //  1.2 PROXIMO
+    // 2. RESGATA DADOS DO USUARIO (USER_INFO) & (USER_EXANS)
+    // 3. REDERIZA A PAGINA USANDO DADOS DE USUARIO
+    var user = token_consut(req.cookies.user_token);
+    var user_data = userdata(user);
 
-    res.status(200).render('users');            // PROCURA ARQUIVO users.hbs
+    if (user_data.user_type == "0"){            // USUARIO ADMINISTRATIVO
+        return res.status(200).render('admin', {user: user_data});
+    }
+    if (user_data.user_type == "1"){            // USUARIO ADMINISTRATIVO
+        return res.status(200).render('users', {user: user_data});
+    }
+    
+
+    console.log(user_data);
+
+    res.status(200).render('users', {user: user_data});
+    //res.status(401).send('Expirado por favor realize novo login');
 })
 
-app.get('*', function(req, res){
+app.get('*', (req, res) => {
     res.status(404).send('what???');            // DEVOLVE ERRO SEM ROTA
 });
 
 // --------------------| FORMULARIOS |-------------------
-app.post('/add_user', function(req, res){
+app.post('/add_user', (req, res) => {
+    // 1. BUSCA SE O USUARIO ESTA CADASTRADO
+    //  1.1 ERRO USUARIO JA TEM CONTA
+    //  1.2 PROXIMO
+    // 2. CADASTRA USUARIO NAS BASES (USERS) (USERS_INFO)
+    // 3. REDIRECIONA PARA 'USERS'
+
     let user_name = req.body.name;
     let user_data = req.body.data;
     let user_addr = req.body.addr;
@@ -130,67 +214,46 @@ app.post('/add_user', function(req, res){
     let user_type = req.body.user_type;
     let user_plan = req.body.user_plan;
     let user_pass = req.body.password;
+
     res.render('home')
 
     // NÃO CONSTRUIDO
 })
 
-// GERA TOKEN DE LOGIN, REDIRECIONA PRA USERS
 app.post('/auth', (req, res) => {
-    var users = JSON.parse(fs.readFileSync(__dirname + '/db/cli.json'))
+    // 1. VERIFICA TOKEN DE LOGIN
+    //  1.1 TOKEN VALIDO -> CONSULTA (TIPO) -> REDIRECIONA 'USERS'
+    //  1.2 TOKEN INVALIDO -> REMOVE ENTRADA DE SESSION -> PROXIMO
+    //  1.3 SEM TOKEN -> PROXIMO
+    // 2. RECEBE OS DADOS DE LOGIN
+    //  2.1 ERRO LOGIN OU SENHA
+    //  2.2 PROXIMO
+    // 3. GERA TOKEN DE LOGIN
+    //  3.1 ERRO TOKEN EM USO -> REPETE 3
+    //  3.2 PROXIMO
+    // 4. INSERE USUARIO E TOKEN EM (USER_SESSIONS)
+    // 5. INSERE NO COOKIE O TOKEN
+    // 6. REDIRECIONA PRA USERS
+
     let username = req.body.username;
-    let password = sha512(req.body.password, process.env.SECRET_USERS)
-    console.log(username, password);
-    
-    if (username && password) {                         // VERIFICA PREENCHIMENTO DOS CAMPOS
-        var userloc = users.find((item) => {
-            return (item.user == username && item.password == password)
-        })
-        console.log(userloc);
-        if (userloc) {                                  // VERIFICA SE ESTA NA BASE DE CLIENTES
-            var rand = Math.random()*1e16;
-            //const token = jwt.sign(
-            //    { userid: userloc.userid }, // payload (podem ser colocadas outras infos)
-            //    process.env.SECRET, // chave definida em .env
-            //    { expiresIn: 300 }  // em segundos
-            //)
-            //return res.json({ auth: true, token });
-            res.cookie('user_token', rand, { expires: new Date(Date.now() + 900000)});
-            
-        }
-        //if (username === 'admin' && password === 'admin') {
-        //
-        //    res.status(201).render('users');        // LOGIN OK
-        //}
-        res.status(200).redirect('users');
+    let password = sha512(req.body.password, process.env.SECRET_USERS);
+
+    userloc = login(username, password);
+
+    if (userloc) {                                  // VERIFICA SE ESTA NA BASE DE CLIENTES
+        var session_token = token_modify(username);
+        res.cookie('user_token', session_token, { expires: new Date(Date.now() + 900000)});  // 15 MIN
+        return res.status(200).redirect('users');   // ENCERRA CAMINHO (SEM ERRO DE REENVIO)
     }
     
-    //res.status(401).end('Login failed');   // FALHA DE LOGIN
+    res.status(401).send('Email ou Senha Incorretos');   // FALHA DE LOGIN
 })
 
 // --------------------| FILE SYSTEM |-------------------
-/*
-fs.readFile('./db/db.json', 'utf8', (err, file_user) =>{
-    if(err) {
-        console.error(err);
-        return;
-    }
-    try {
-        const data = JSON.parse(file_user);
-        var keys = Object.keys(data);
 
-        for (var i = 0; i < keys.length-1; i++) {
-            console.log(data[keys[i]][1]);      // DATA[CHAVE][LINHA]
-        }
-    } catch(err) {
-        console.error(err)
-    }
-})
-*/
+
 // --------------------| OUVE PORTA |--------------------
 app.listen(PORTA, () => {
     // LOG
     console.log("Servidor rodando na porta: %d", PORTA);
-    //console.log(process.env) 
-
 });
